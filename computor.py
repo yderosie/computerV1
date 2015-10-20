cb5976db8a305bd73bbb33dbb0c1afda6d04500d
Yohann DEROSIER <yderosie@e3r7p3.42.fr>
2015-10-20 18:40:42 +0200

    computor.py

--- computor.py
+++ computor.py
@@ -27,58 +27,34 @@ def enter():
 	regex = re.compile("((?:^\s*([+-])?|\s*(?<![+-])([+-]))\s*(?:(\d+(?:\.\d*)?)\s*\*)?\s*(?<![+-])([+-])?(\d+(?:\.\d*)?)?([xX])?(?:\^(\d+))?\s*)+=((?:^\s*([+-])?|\s*(?<![+-])([+-]))|(?:(\d+(?:\.\d*)?)\s*\*)?\s*(?<![+-])([+-])?(\d+(?:\.\d*)?)?([xX])?(?:\^(\d+))?\s*)+")
 	equation = str(sys.argv[1]).replace(' ', '')
 	if (regex.match(equation)):
-		print 'ok'
-		i = j = 0
-		while i < len(equation):
-			if equation[i] == '^' and int(equation[i + 1]) > 2 and int(equation[i + 1] >= 0):
-				print "Equation de degre trop elevee."
+		j = 0
+		parsing = re.split(r'[+=-]', equation)
+		for var in parsing:
+			if (int(var.split('^')[1]) > j):
+				j = int(var.split('^')[1])
+			if (int(var.split('^')[1]) > 2):
+				print "The polynomial degree is stricly greater than 2, I can't solve."
 				return
-			if equation[i] == '^' and int(equation[i + 1]) > j:
-				j = int(equation[i + 1])
-			i += 1
 	else:
 		print "Le paramettre entrer ne correspond pas a une equation."
 		return
-	
+
 	print equation
 	liste = equation.split('=')
 	equ = re.split(r'[+-]', liste[0])
-	#c = ax = bx = 0
-	tab = [(0,""), (1,""), (2,"")]
-	print tab
+	tab = [0.0, 0.0, 0.0]
+	tab2 = [0.0, 0.0, 0.0]
 	for var in equ:
-#		print var
-		tab[var.split('^').[1]] = var.split('*').[0]
-
-	#	if var.count('x^0') or var.count('X^0'):
-	#		c = var[::-1]
-	#		c = c[4:]
-	#		c = c[::-1]
-	#	elif var.count('x^1') or var.count('X^1'):
-	#		bx = var[::-1]
-	#		bx = bx[4:]
-	#		bx = bx[::-1]
-	#	elif var.count('x^2') or var.count('X^2'):
-	#		#ax = var[::-1]
-	#		#ax = ax[4:]
-	#		#ax = ax[::-1]
-	#		ax = var[:-4]
-	print tab
-#	print equ
-	if liste[1].find('X^0') or liste[1].find('x^0'):
-		egale = liste[1][::-1]
-		egale = egale[4:]
-		egale = egale[::-1]
-#	print liste
-#	print egale
-#	print ax
-#	print bx
-#	print c
-	c = int(c) - int(egale)
-#	print c
-	print ("Reduced form: "+ str(c) +" + "+ str(bx) +" * X + "+ str(ax) +" * X^2 = 0")
+		tab[int(var.split('^')[1])] = float(var.split('*')[0])
+	egale = re.split(r'[+-]', liste[1])
+	for var in egale:
+		tab2[int(var.split('^')[1])] = float(var.split('*')[0])
+	tab[0] = tab[0] - tab2[0]
+	tab[1] = tab[1] - tab2[1]
+	tab[2] = tab[2] - tab2[2]
+	print ("Reduced form: "+ str(tab[0]) +" + "+ str(tab[1]) +" * X + "+ str(tab[2]) +" * X^2 = 0")
 	print ("Polynomial degree:" + str(j))
-	delta(ax,bx,c)
+	delta(tab[2],tab[1],tab[0])
 	#parsing(equation)
 
 enter()
