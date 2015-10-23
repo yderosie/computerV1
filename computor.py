d50f5e9141321499974ffb606854e358d9e919c2
Yohann DEROSIER <yderosie@e3r7p4.42.fr>
2015-10-23 12:56:35 +0200

    computor.py

--- computor.py
+++ computor.py
@@ -23,12 +23,13 @@ import re
 def delta(tab):
 	d = (tab[1] * tab[1]) - 4 * tab[0] * tab[2]
 	if d > 0:
+		print "Discriminant is strictly positive, the two solutions are:"
 		sup()
 	elif d == 0:
 		egal()
 	else:
-		
- 
+		pass
+
 def enter():
 	solve = True
 	regex = re.compile("((?:^\s*([+-])?|\s*(?<![+-])([+-]))\s*(?:(\d+(?:\.\d*)?)\s*\*)?\s*(?<![+-])([+-])?(\d+(?:\.\d*)?)?([xX])?(?:\^(\d+))?\s*)+=((?:^\s*([+-])?|\s*(?<![+-])([+-]))|(?:(\d+(?:\.\d*)?)\s*\*)?\s*(?<![+-])([+-])?(\d+(?:\.\d*)?)?([xX])?(?:\^(\d+))?\s*)+")
@@ -42,14 +43,14 @@ def enter():
 			if (int(var.split('^')[1]) > 2):
 				solve = False	#print "The polynomial degree is stricly greater than 2, I can't solve."
 	else:
-		print "Le paramettre entrer ne correspond pas a une equation."
+		print "The paramettre entering is not an equation ."
 		return
 	print equation
 	liste = equation.split('=')
 	equ = re.split(r'[+-]', liste[0])
 	tab = []
 	i = 0
-	while j < i:
+	while j >= i:
 		tab.append(0.0)
 		i += 1
 	for var in equ:
@@ -57,6 +58,11 @@ def enter():
 	egale = re.split(r'[+-]', liste[1])
 	for var in egale:
 		tab[int(var.split('^')[1])] -= float(var.split('*')[0])
+	print tab
+	for t in enumerate(tab):
+		if t:
+			print str(t[1]) + " * x^" + str(t[0]) + (" + " if int(t[0]) != j else ''),
+	print ("= 0")
 	print ("Reduced form: "+ str(tab[0]) +" + "+ str(tab[1]) +" * X + "+ str(tab[2]) +" * X^2 = 0")
 	print ("Polynomial degree:" + str(j))
 	if not solve:
